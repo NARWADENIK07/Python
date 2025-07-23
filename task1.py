061fe5b0cff293c7cd189aec23597f88ebfc770f
Nikhil Narwade <narwadenikhil6939@gmail.com>
2025-07-23 12:11:38 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+a=input('Enter the frist numbe: ')
+a=int(a)
+b=input('Enter the Second number: ')
+b=int(b)
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+
+print("Addition: ",c)
+print("Subtraction: ",d)
+print("Multiplication : ",e)
+print("Division: ",f)
---
+++ task2.py
@@ -0,0 +1,5 @@
+a=input('Enter your frist name: ')
+b=input('Enter your last name: ')
+fn=a+" "+b
+
+print('Hello, '+fn+'! Welcome to the Python program.')
